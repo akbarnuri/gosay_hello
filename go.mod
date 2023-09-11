44f8291034c9b003d72c6e4af9cf3c41cc580bd6
akbarnuri <makerctvvip@gmail.com>
2023-09-12 05:16:49 +0700

    update say hello with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/akbarnuri/gosay_hello
+module github.com/akbarnuri/gosay_hello/v2
 
 go 1.21.1
--- gosay_hello.go
+++ gosay_hello.go
@@ -1,5 +1,5 @@
 package gosay_hello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello World" + name
 }
